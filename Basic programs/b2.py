1edc9c9f94bbdfc501e4bf0db4dad9a56fdccfd3
Devatharani G S <130377425+devags2005@users.noreply.github.com>
2023-06-12 14:12:28 +0000

    2.py

--- Basic programs/b2.py
+++ Basic programs/b2.py
@@ -5,8 +5,8 @@ Surface area of a cylinder = ((2*pi*radius)*height) + ((pi*radius**2)*2)'''
 
 r=int(input("Enter radius:"))
 h=int(input("Enter height:"))
-def cylinder(r,h):
+def c(r,h):
     volume=3.14*r*r*h
     surfacearea=(2*3.14*r*h)+(2*3.14*r*r)
     print("volume and area is:",volume,surfacearea)
-cylinder(r,h)
+c(r,h)
