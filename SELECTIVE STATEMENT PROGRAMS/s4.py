10b3ea59de6a6f4caf0da7b7ed3da3de50583158
Devatharani G S <130377425+devags2005@users.noreply.github.com>
2023-06-12 15:58:59 +0000

    7.py

--- SELECTIVE STATEMENT PROGRAMS/s4.py
+++ SELECTIVE STATEMENT PROGRAMS/s4.py
@@ -12,4 +12,4 @@ if(2023-year)>=60 and age>=60:
     t=1020*(20/100)
     print("Your omni bus charge is:",t)
 else:
-     print("Your omni bus charge is 1020")
+    print("Your omni bus charge is 1020")
