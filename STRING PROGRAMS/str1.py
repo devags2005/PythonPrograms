6c3f8d22e5014bbda0ee47d3c485efea12004134
Devatharani G S <130377425+devags2005@users.noreply.github.com>
2023-06-18 03:50:44 +0000

    String made of program

---
+++ STRING PROGRAMS/str1.py
@@ -0,0 +1,9 @@
+'''Write a Python program to get a string made of the first 2 and last 2 characters of a
+given string. If the string length is less than 2, return the empty string instead.'''
+
+str1=input("Enter the string: ")
+if len(str1)<=2:
+    print(" ")
+else:
+    str2=str1[0:2]+str1[-2:]
+    print(str2)
