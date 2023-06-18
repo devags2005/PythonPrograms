3f7ab24fc9e9137354a67e59b997c8a660561afc
Devatharani G S <130377425+devags2005@users.noreply.github.com>
2023-06-18 03:42:36 +0000

    Fibonacci series using recursion function

---
+++ FUNCTIONS PROGRAMS/f4.py
@@ -0,0 +1,13 @@
+'''Write a Python code to find the nth term of the Fibonacci series using RECURSION'''
+
+def func(n):
+    if n<= 0:
+        print("Incorrect input")
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return (func(n-1) + func(n-2))
+n=int(input("Enter nth term:"))
+print(func(n))
