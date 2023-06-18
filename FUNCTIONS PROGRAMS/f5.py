1a556a67ee9878814bd6e2c79b062083cb277b1d
Devatharani G S <130377425+devags2005@users.noreply.github.com>
2023-06-18 03:44:04 +0000

    Calculating power of numbers

---
+++ FUNCTIONS PROGRAMS/f5.py
@@ -0,0 +1,9 @@
+'''Write a Python function that takes base and power as arguments and calculates the
+power of numbers.'''
+
+def power(b,p):
+    po=(b**p)
+    print(po)
+b=int(input("Enter base number: "))
+p=int(input("Enter power: "))
+power(b,p)
